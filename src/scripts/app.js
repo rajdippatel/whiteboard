ec3bebe22c5f7df1dce7ab0fd89ec7acbeee9b42
RAJ PATEL <raj.rajdip89@gmail.com>
2018-03-16 13:08:38 +0530

    Add documentation and comments.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,31 +1,48 @@
-﻿var whiteboard;
+﻿/*
+Author : Rajdip Patel (raj.rajdip89@gmail.com)
+
+Whiteboard public api test file.
+
+*/
+
+
+var whiteboard;
+/* Register page onload event to initialize whiteboard object globally */
 window.onload = function () {
     whiteboard = new WhiteBoard("whiteboard");    
 }
 
+/* Sets Rangle shape type on global whiteboard object */
 function onRectableButtonClick() {
     whiteboard.setShapeType(WhiteBoard.ShapeType.Rectangle);
 }
 
+/* Sets FreeHand shape type on global whiteboard object */
 function onFreeHandButtonClick() {
     whiteboard.setShapeType(WhiteBoard.ShapeType.FreeHand);
 }
 
+/* Sets Arrway shape type on global whiteboard object */
 function onArrowButtonClick() {
     whiteboard.setShapeType(WhiteBoard.ShapeType.Arrow);    
 }
 
+/* Performs undo operation on global whiteboard object */
 function onUndoButtonClick() {
     whiteboard.undo();
 }
 
+/* Performs redo operation on global whiteboard object */
 function onRedoButtonClick() {
     whiteboard.redo();
 }
 
+/* Clears global whiteboard object, so all previously drawn area will be erased */
 function onClearButtonClick() {
     whiteboard.clear();
 }
+
+/* Changes color for next drawing on global whiteboard object */
 
 function onColorButtonClick(color) {
     whiteboard.setColor(color);
--- src/scripts/whiteboard.js
+++ src/scripts/whiteboard.js
@@ -1,22 +1,80 @@
-﻿(function (scope) {
+﻿/*
+Author : Rajdip Patel (raj.rajdip89@gmail.com)
 
+Whiteboard js library.
+
+Supported Operations/Modes:
+
+- Shape Types :
+1) Rectangle
+2) FeeHand
+3) Arrow with triangle head
+
+- Operation Types :
+1) Color types
+2) Undo/Redo support
+3) Clear/Draw everything
+
+Example Usage :
+
+var whiteboard = new WhiteBoard("canvasId");
+OR
+var whiteboard = new WhiteBoard(canvasElement);
+OR
+var whiteboard = new WhiteBoard();
+
+var canvasElement = whiteboard.getElement();
+
+whiteboard.setColor('color');
+
+whiteboard.setShapeType(WhiteBoard.ShapeType.Rectangle);
+whiteboard.setShapeType(WhiteBoard.ShapeType.FreeHand);
+whiteboard.setShapeType(WhiteBoard.ShapeType.Arrow); 
+
+whiteboard.undo();
+whiteboard.redo();
+
+whiteboard.clear();
+
+*/
+
+/* Self-enclosed closure pattern to support custom scope names.
+   Default scope will be global window object.
+*/
+(function (scope) {
+
+    // Antialising offset to draw sharp lines/rectangles. 
+    // Without this offset lines and rectangles will be blurry.       
     var ALIAS_OFFSET = 0.5;
 
+    // Shape type enumuration
     var ShapeType = {
         FreeHand: 1,
         Arrow: 2,
         Rectangle: 3,
     };
 
+    /* Point Class:-
+        x: X-Axis coordinate
+        y: Y-Axis coordinate
+    */
     var Point = function (x, y) {
         this.x = x;
         this.y = y;
     }
 
+    // Returns duplicated point object from existing values.
+    // It will be useful to modify existing point object by keeping original object intact.
     Point.prototype.clone = function () {
         return new Point(this.x, this.y);
     }
 
+    /* Rectangle Class:-
+        x: X-Axis coordinate
+        y: Y-Axis coordinate
+        width: Rectangle width
+        height : Rectangle height
+    */
     var Rect = function (x, y, width, height) {
         this.x = x;
         this.y = y;
@@ -24,50 +82,97 @@
         this.height = height;
     }
 
+    /*
+        Shape Class:-
+        type: ShapeType
+        color: String
+        points: Array of points representing pixel co-ordinates
+    */
     var Shape = function (type, color, points) {
         this.type = type;
         this.color = color,
         this.points = points || [];
     }
 
-    var WB = function (canvas) {
+    /*
+        WhiteBoard Class:-
+        [canvas]: canvas id or canvas element
+
+        Main whiteboard class to draw shapes, hanlding events. 
+        If it is initilized with canvas id it will fetch canvas element from html dom.
+        If it it initilized with canvas element it will try to utilize that canvas element.
+        In other cases it will create its own canvas element to render shapes. It is caller's responsibilty to add canvas into dom at appropriate places.
+    */
+    var WhiteBoard = function (canvas) {
         if (typeof canvas === 'string') {
             canvas = document.getElementById(canvas);
         } 
 
+        // If canvas is not specified, create canvas element dynamically.
         this.canvas = canvas || document.createElement("canvas");
+        // Store canvas context object, it will speedup performance while mouse/touch events
         this.ctx = this.canvas.getContext("2d");
+        // Apply default drawing styles
         this.ctx.lineWidth = "1";
 
+        // Shapes array to hold each and every shape which is drawn earlier on this whiteboard
         this.shapes = [];
+        
+        // Shapes array to keep undo done shapes
         this.undoShapes = [];
+        
+        // Holds reference to on-going shape, useful for interactive events
         this.activeShape = null;
+        
+        // Holds already registered events, useful for unbinding events once shape drawing finish
         this.events = {};
+        
+        // Canvas element's client bouding rect, useful for interactive events
         this.clientRect = null;
+
+        // Whiteboard uses clipped redrawing way instead of re-drawing every shape on interactive events. Useful to achieve > 60fps frame rate on interactive events. 
         this.dirtyData = null;
         this.dirtyRect = null;
 
+        // Stores current selected shape attribute selected by user.
         this.currentShapeType = ShapeType.Rectangle;
         this.currentColor = "#000000";
 
         this.init();        
     }
 
-    var proto = WB.prototype;
+    // Shorthand to define prototype based methods.
+    var proto = WhiteBoard.prototype;
 
+    /*
+        getElement Method:-
+        Returns: canvas element
+    */
     proto.getElement = function () {
         return this.canvas;
     }
 
+    /*
+        getContext Method:-
+        Returns: Drawing context for canvas object
+    */
     proto.getContext = function () {
         return this.ctx;
     }
 
+    /*
+        init Method:-
+        Whiteboard initialization
+    */
     proto.init = function () {
         this.initSize();
         this.attachEvents();
     }
 
+    /*
+        initSize Method:-
+        Initialize canvas properties for HI-DPI (Retina) devices like MacBook, Mobiles Devices
+    */
     proto.initSize = function() {
         if(devicePixelRatio != 1) {
             var canvas = this.canvas;
@@ -82,6 +187,7 @@
             canvas.style.width = width + "px";
             canvas.style.height = height + "px";
 
+            // Scale canvas context, so all drawing operation will be adjusted accordingly.
             ctx.scale(devicePixelRatio, devicePixelRatio);
         }
     }
@@ -94,10 +200,20 @@
         this.currentColor = color;
     }
 
+    /*
+        attachEvents Method:-
+        Registers only mousedown event on canvas element. 
+        After mousedown event it will register mousemove, mouseup event on document elemenet. 
+        So it will continue to receive mouse events even if cursor will go outside of canvas element, browser window etc.
+    */
     proto.attachEvents = function () {
         this.registerEvent(this.canvas, "mousedown", this.onMouseDown);
     }
 
+    /*
+        clear Method:-
+        Clears all shapes and resets it to original state
+    */
     proto.clear = function () {
         this.shapes = this.undoShapes = [];
         this.drawShapes();
@@ -112,6 +228,10 @@
         delete this.events[eventType];
     }
 
+    /*
+        onMouseDown Event Handler:-
+        Handles mouse down event from canvas element. It creates new shape object and adds to shapes array.
+    */
     proto.onMouseDown = function (e) {
         if (this.currentShapeType) {
             this.registerEvent(document, "mousemove", this.onMouseMove);
@@ -121,10 +241,20 @@
         }
     }
 
+    /*
+        onMouseMove Event Handler:-        
+        Handles mouse move event from document element. Redraws current shape according to cursor position.
+        e: Event object
+    */
     proto.onMouseMove = function (e) {
         this.processEvent(e, this.backupPixelRect.bind(this));
     }
 
+    /*
+        onMouseUp Event Handler:-
+        Handles mouse up event from document element. Redraws current shape according to cursor position. Finish drawing for current shape.
+        e: Event object
+    */
     proto.onMouseUp = function (e) {
         this.processEvent(e);
         this.unRegisterEvent(document, "mousemove");
@@ -133,6 +263,12 @@
         this.activeShape = null;
     }
 
+    /*
+        processEvent Method:-
+        Processes mousemove/up event handlers. 
+        e: Event object
+        [pixelBackupHandler]: If specified it will call that function to backup canvas surface before active shape will start
+    */
     proto.processEvent = function (e, pixelBackupHandler) {
         if(this.activeShape != null) {
             switch (this.activeShape.type) {
@@ -144,11 +280,15 @@
                     this.activeShape.points.push(this.getOffsetPoint(e));
                     break;
             }
-            this.eraseDirtyRect();
+            this.restoreDirtyRect();
             this.drawShape(this.activeShape, pixelBackupHandler);
         }        
     }
 
+    /*
+        drawShapes Method:-
+        Redraws all shapes on canvas after clearing it.
+    */
     proto.drawShapes = function () {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         for (var i = 0; i < this.shapes.length; i++) {
@@ -156,6 +296,12 @@
         }
     }
 
+    /*
+        drawShape Method:-
+        Draws single shape on canvas surface.
+        shape: Shape to draw
+        pixelBackupHandler: Callback function to backup canvas surface 
+    */
     proto.drawShape = function (shape, pixelBackupHandler) {
         this.ctx.strokeStyle = this.ctx.fillStyle = shape.color;
         switch (shape.type) {
@@ -171,6 +317,12 @@
         }
     }
 
+    /*
+        drawArrow Method:-
+        Draws array with triangle head.
+        shape: Arrow shape to draw
+        pixelBackupHandler: Callback function to backup canvas surface 
+    */
     proto.drawArrow = function (shape, pixelBackupHandler) {
         var points = shape.points;        
         if (pixelBackupHandler) {
@@ -191,6 +343,12 @@
         drawArrowhead(this.ctx, points[1].x, points[1].y, radians, shape.color);
     }
 
+    /*
+        drawRectangle Method:-
+        Draws rectangle.
+        shape: Rectangle shape to draw
+        pixelBackupHandler: Callback function to backup canvas surface 
+    */
     proto.drawRectangle = function (shape, pixelBackupHandler) {
         var points = shape.points;
         var drawingRect = getMaxBoudingRect(points);
@@ -202,6 +360,11 @@
         this.ctx.closePath();
     }
 
+    /*
+        drawFreeHand Method:-
+        Draws freehand shape.
+        shape: Freehand shape to draw        
+    */
     proto.drawFreeHand = function (shape) {
         var points = shape.points;
         this.ctx.beginPath();
@@ -213,22 +376,40 @@
         this.ctx.closePath();        
     }
 
+    /*
+        backupPixelRect Method:-
+        Backups canvas surface pixel data before drawing temporary shape. Temporary shapes will be drawn only with mousemove events.
+        rect: Area to backup
+    */
     proto.backupPixelRect = function (rect) {
         this.dirtyRect = rect;
         this.dirtyData = this.ctx.getImageData(this.dirtyRect.x, this.dirtyRect.y, this.dirtyRect.width, this.dirtyRect.height);
     }
 
-    proto.eraseDirtyRect = function () {
+    /*
+        restoreDirtyRect Method:-
+        Restores previous canvas surface backup to clear dirty shape drawing
+    */
+    proto.restoreDirtyRect = function () {
         if (this.dirtyData && this.dirtyRect) {
             this.ctx.putImageData(this.dirtyData, this.dirtyRect.x, this.dirtyRect.y);
             this.dirtyData = this.dirtyRect = null;
         }
     }
 
+    /*
+        getOffsetPoint Method:-
+        Converts mouse event co-oridinates to canvas element's offset. It is useful to convert offsets even when mouse event is generated from outside of canvas element or browser window.
+        Returns: offset point on canvas element
+    */
     proto.getOffsetPoint = function (e) {
         return new Point(e.clientX - this.clientRect.left, e.clientY - this.clientRect.top);
     }
 
+    /*
+        undo Method:-
+        Performs undo operation by removing last drawn shape from surface
+    */
     proto.undo = function () {
         if (this.shapes.length > 0) {
             this.undoShapes.push(this.shapes.pop());
@@ -237,6 +418,10 @@
         }
     }
 
+    /*
+        redo Method:
+        Performs redo operation by adding last undo shape into surface
+    */
     proto.redo = function () {
         if (this.undoShapes.length > 0) {
             this.shapes.push(this.undoShapes.pop());
@@ -245,6 +430,11 @@
         }
     }
 
+    /*
+        getMaxBoudingRect Method:-
+        Utility method to get surrounding bound rectange for all given points.
+        points: Array of points object
+    */
     function getMaxBoudingRect(points) {
         var pointMin = points[0].clone();
         var pointMax = points[0].clone();
@@ -266,6 +456,15 @@
         return new Rect(pointMin.x, pointMin.y, pointMax.x - pointMin.x, pointMax.y - pointMin.y);
     }
 
+    /*
+        drawArrowhead Method:-
+        Utility method to draw arrow head triangle.
+        ctx: canvas context
+        x: X-Axis co-ordinate
+        y: Y-Axis co-ordinate
+        radians: Slope of array head triangle
+        color: Fill and Stroke color
+    */
     function drawArrowhead(ctx, x, y, radians, color) {
         ctx.save();
         ctx.beginPath();
@@ -279,6 +478,8 @@
         ctx.fill();
     }
 
-    WB.ShapeType = ShapeType;
-    scope.WhiteBoard = WB;
-})(window);
+    // Expose Shape utility on whiteboard namespace
+    WhiteBoard.ShapeType = ShapeType;
+    // Expose whiteboard on requested scope or on global window object
+    (scope || window).WhiteBoard = WhiteBoard;
+})();
